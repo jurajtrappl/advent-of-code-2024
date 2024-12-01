c56062817c88236703be2ea3d1b8ada17fc39f12
Juraj Trappl <juraj.trappl@proton.me>
2024-12-01 10:57:15 +0100

    changed to signed + abs

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -8,8 +8,8 @@
 
 use aoe::{count_occurrences, parse_to_2dvec, read_input_file};
 
-fn split_location_lists(locations: &Vec<Vec<u32>>) -> (Vec<u32>, Vec<u32>) {
-    let (mut fst, mut snd): (Vec<u32>, Vec<u32>) = locations
+fn split_location_lists(locations: &Vec<Vec<i32>>) -> (Vec<i32>, Vec<i32>) {
+    let (mut fst, mut snd): (Vec<i32>, Vec<i32>) = locations
         .iter()
         .map(|l| match l.as_slice() {
             [a, b, ..] => (*a, *b),
@@ -23,24 +23,24 @@ fn split_location_lists(locations: &Vec<Vec<u32>>) -> (Vec<u32>, Vec<u32>) {
     (fst, snd)
 }
 
-fn find_distance(fst_locations: &[u32], snd_locations: &[u32]) -> u32 {
+fn find_distance(fst_locations: &[i32], snd_locations: &[i32]) -> i32 {
     fst_locations
         .iter()
         .zip(snd_locations.iter())
-        .map(|(fst, snd)| if fst > snd { fst - snd } else { snd - fst })
+        .map(|(fst, snd)| (fst - snd).abs())
         .sum()
 }
 
-fn calculate_similarity_score(fst_locations: &[u32], snd_locations: &[u32]) -> u32 {
+fn calculate_similarity_score(fst_locations: &[i32], snd_locations: &[i32]) -> i32 {
     let counter = count_occurrences(snd_locations);
     fst_locations.iter().fold(0, |acc, elem| {
-        acc + elem * counter.get(elem).map_or(0, |&count| count as u32)
+        acc + elem * counter.get(elem).map_or(0, |&count| count as i32)
     })
 }
 
-fn solve(input: &str, op: fn(&[u32], &[u32]) -> u32) -> u32 {
-    let locations: Vec<Vec<u32>> = parse_to_2dvec(input);
-    let (fst_locations, snd_locations): (Vec<u32>, Vec<u32>) = split_location_lists(&locations);
+fn solve(input: &str, op: fn(&[i32], &[i32]) -> i32) -> i32 {
+    let locations: Vec<Vec<i32>> = parse_to_2dvec(input);
+    let (fst_locations, snd_locations): (Vec<i32>, Vec<i32>) = split_location_lists(&locations);
     op(&fst_locations, &snd_locations)
 }
 
